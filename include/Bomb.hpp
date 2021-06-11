b7487a2968ad31d3254d4ad49fe7920a5005f3a5
Adrian Lewitzki <alewitzki@localhost.localdomain>
2021-06-11 20:41:09 +0200

    restructure bomb code

--- include/Bomb.hpp
+++ include/Bomb.hpp
@@ -19,52 +19,11 @@ protected:
     GameEventReceiver *receiver;
 
 public:
-    explicit Bomb(SAppContext &ctx)
-    {
-        context = &ctx;
-        this->smgr = context->device->getSceneManager();
-        this->driver = context->device->getVideoDriver();
-
-        irr::scene::IAnimatedMesh *mesh;
-
-        if ((mesh = smgr->getMesh("media/bomb.3ds")) == NULL) {
-            AssetLoadErrorMac("Can't load 'media/bomb.3ds'");
-        }
-        if ((this->body = smgr->addAnimatedMeshSceneNode(mesh)) == NULL) {
-            SceneErrorMac("Could not add AnimatedMeshSceneNode");
-        }
-
-        this->body->setMaterialFlag(irr::video::EMF_LIGHTING, false);
-        this->body->setMaterialTexture(0, driver->getTexture("media/sydney.bmp"));
-        this->body->setScale(irr::core::vector3df(.1));
-
-        this->selector = this->smgr->createOctreeTriangleSelector(this->body->getMesh(), this->body, 128);
-        this->body->setTriangleSelector(this->selector);
-    }
-
-    void drop(const irr::core::vector3df &pos)
-    {
-        this->body->setPosition(pos);
-        this->body->setVisible(true);
-        // @todo start timer ... explode etc.
-    }
-
+    explicit Bomb(SAppContext &ctx);
     ~Bomb() override = default;
-    void setPosition(float x, float z) final {
-        this->body->setPosition(irr::core::vector3d<irr::f32>(x, 45, z));
-    }
-
-    bool HandleCollision(Player &player) override
-    {
-        if (player.getBody()->getTransformedBoundingBox().intersectsWithBox(
-                    this->body->getTransformedBoundingBox()))
-        {
-            this->body->setVisible(false);
-            std::cout << "hit" << std::endl;
-            return true;
-        }
-        return false;
-    }
+    void drop(const irr::core::vector3df &pos);
+    void setPosition(float x, float z) final;
+    bool HandleCollision(Player &player) override;
 };
 
 #endif //B_YEP_400_BER_4_1_INDIESTUDIO_KARL_ERIK_STOERZEL_BOMB_HPP
