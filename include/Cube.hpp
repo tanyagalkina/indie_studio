4fa7f7d570a335e6b8a254829b332f21e51a3cfa
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-06-20 20:57:58 +0200

    marmor palace

--- include/Cube.hpp
+++ include/Cube.hpp
@@ -4,10 +4,10 @@
 #include "IXML.hpp"
 //                     meshPath       texturePath
 static const std::pair<irr::io::path, irr::io::path>  assetPath[7] = {
-    std::make_pair("media/WoodenBox.3ds", "media/textures/grass.png"), // Empty
-    std::make_pair("media/WoodenBox.3ds", "media/WoodPlanks_Albedo.png"), // Box
+    std::make_pair("media/WoodenBox.3ds", "media/marmor2.png"), // Empty
+    std::make_pair("media/WoodenBox.3ds", "media/marmor.png"), // Box
     //std::make_pair("media/WoodenBox.3ds", "media/textures/grass.png"), // Tile
-    std::make_pair("media/WoodenBox.3ds", "media/textures/wall_interior_save.png"), // Wall
+    std::make_pair("media/WoodenBox.3ds", "media/marmor4.png"), // Wall
     //std::make_pair("media/WoodenBox.3ds", "media/textures/wall_interior_save.png"), // Wall_Boss
     //std::make_pair("media/WoodenBox.3ds", "media/textures/wall_interior_save.png"), // Boss
     //std::make_pair("media/WoodenBox.3ds", "media/WoodPlanks_Normal.png"), // Player starting pos
